559135111054432a5b398f92c828b36781476bb3
Mykezero <as482jtm45@aol.com>
2016-03-26 16:00:12 -0400

    Started writing the new profile management system.
    We'll be support multiple characters running at the same time,
    so the old, static config config singleton will be replaced with a
    Profile for each user.

--- test/EasyFarm.Tests/UnitTest1.cs
+++ test/EasyFarm.Tests/UnitTest1.cs
@@ -1,41 +1,95 @@
-﻿using System.Diagnostics;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.IO;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoMoq;
+using Ploeh.AutoFixture.Xunit2;
+using Xunit;
 
 namespace EasyFarm.Tests
 {
-    [TestClass]
-    public class UnitTest1
+    public class ProfileTests
     {
-        [TestMethod]
-        public void LoadsProfileWithCorrectName()
+        public class Create
         {
-            var controller = new Controller();
-            var profile = controller.LoadProfile("Mike");
-            Assert.AreEqual("Mike", profile.Name);
-        }
+            [Theory, AutoMoqData]
+            public void CreatesProfileWithGivenName(Controller controller)
+            {
+                var profile = controller.CreateProfile("Mike");
+                Assert.Equal("Mike", profile.Name);
+            }
 
-        [TestMethod]
-        public void SetsProcessForProfile()
+            [Theory, AutoMoqData]
+            public void SaveProfileWithChanges(
+                Controller controller, 
+                Profile profile)
+            {
+                var result = controller.SaveProfile(profile);
+                Assert.True(result.Success);
+            }
+        }
+    }
+
+    public class ProfileManager
+    {
+        public Result Save(Profile profile)
         {
-            var controller = new Controller();
-            var profile = controller.LoadProfile("Mike");
-            var process = new Process();
-            profile.Process = process;
-            Assert.AreEqual(profile.Process, process);
+            if (File.Exists(profile.Name)) return Result.Fail("File already exists");
+            return Result.Ok();
         }
     }
 
     public class Controller
     {
-        public Profile LoadProfile(string profile)
+        private readonly ProfileManager _profileManager;
+
+        public Controller(ProfileManager profileManager)
         {
-            return new Profile() {Name = profile};
+            _profileManager = profileManager;
+        }
+
+        public Profile CreateProfile(string profileName)
+        {
+            return new Profile { Name = profileName };
+        }
+
+        public Result SaveProfile(Profile profile)
+        {
+            return _profileManager.Save(profile);
+        }
+    }
+
+    public class Result
+    {
+        public bool Success { get; }
+
+        public string Message { get; private set; }
+
+        public Result(bool success, string message = "")
+        {
+            Success = success;
+            Message = message;
+        }        
+
+        public static Result Fail(string message)
+        {
+            return new Result(false, message);
+        }
+
+        public static Result Ok()
+        {
+            return new Result(true);
         }
     }
 
     public class Profile
     {
         public string Name { get; set; }
-        public Process Process { get; set; }
+    }
+
+    public class AutoMoqDataAttribute : AutoDataAttribute
+    {
+        public AutoMoqDataAttribute() :
+            base(new Fixture().Customize(new AutoMoqCustomization()))
+        {
+        }
     }
 }
