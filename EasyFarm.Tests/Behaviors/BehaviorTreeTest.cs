bc33ad4a74aca20c20523cf41ec26d766486d8a0
Mykezero <mikebartron@gmail.com>
2016-09-19 23:13:03 -0400

    Add blank behaviors for behavior trees
    Add blank behaviors for new behavior tree implementation.

---
+++ EasyFarm.Tests/Behaviors/BehaviorTreeTest.cs
@@ -0,0 +1,29 @@
+﻿namespace EasyFarm.Tests.Behaviors
+{    
+    public class BehaviorTreeTest { }    
+
+    public enum Status
+    {
+        Success,
+        Failure,
+        Running
+    }
+
+    public class BehaviorTree { }
+    public class Composite { }
+    public class Decorator { }
+    public class Leaf { }
+    public class Sequence { }
+    public class Selector { }
+    public class RandomSequence { }
+    public class RandomSelector { }
+    public class Inverter { }
+    public class Succeeder { }
+    public class Repeater { }
+    public class RepeatUntilFail { }
+    public class PushToStack { }
+    public class PopFromStack { }
+    public class IsEmpty { }
+    public class SetVariable { }
+    public class IsNull { }        
+}
